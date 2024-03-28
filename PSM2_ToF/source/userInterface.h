66f63e22345b725b34f1649e8f58fdee7d086bb1
JaKaczocha <jakub.kaczocha44295@gmail.com>
2024-03-28 03:21:25 +0100

    second version of the user interface

--- PSM2_ToF/source/userInterface.h
+++ PSM2_ToF/source/userInterface.h
@@ -1,7 +1,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-uint8_t updateMode(uint8_t * const mode, bool* const nextMode, const uint16_t modeCounter);
-uint8_t updateValue(const uint8_t mode, int8_t * const changeDirection,int16_t * const colorMode,uint16_t * 
-    const minValue, uint16_t * const maxValue, uint8_t * const valueJump, const uint16_t colorModeCounter, const uint16_t modeCounter);
-uint8_t displayInterface(char* const buff,const char* const colorModeName,const uint16_t minValue, const uint16_t maxValue, const uint8_t valueJump,const uint8_t mode);
+struct Settings;
+
+void interfaceActivity(volatile struct Settings* const settings,volatile uint16_t* const clicked,volatile int16_t* const rotation );
+void updateMaxValue(volatile struct Settings* const settings,volatile uint16_t* const clicked,volatile int16_t* const rotation);
+void updateMinValue(volatile struct Settings* const settings,volatile uint16_t* const clicked,volatile int16_t* const rotation);
+void updateColorMode(volatile struct Settings* const settings,volatile uint16_t* const clicked,volatile int16_t* const rotation);
+
+void displaySettings(volatile struct Settings* const settings, uint8_t choosenSetting, uint8_t updateMin,uint8_t updateMax,bool selected);
